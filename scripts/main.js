7fa147443ae0ca953817f294a49862b18235a7de
Haley513 <513.h.park@gmail.com>
2024-07-05 19:09:17 +0900

    스타일수정및js작성중

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+document. addEventListener("DOMContentLoaded", function() {
+  const todoTitle = document.getElementById("todo-title");
+  const todoDetails = document.getElementById("todo-details");
+  const todopriority = document.getElementById("todo-priority");
+  const addTodoButton = document.getElementById("add-todo");
+  const todoList = document.getElementById("todo-list");
+
+  const modal = document.getElementById("modal");
+  const closeModalButton = document.getElementsByClassName("modal-title");
+  
+});
